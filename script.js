eb9f96c04daa1f6f89b129427633ae998f8c08a0
Agyapal Singh <agyapalsinghaujla@gmail.com>
2025-01-02 18:03:45 +0530

    Section Titles added

--- script.js
+++ script.js
@@ -169,26 +169,26 @@ tl_product_ingredient.from(".ingrdient-card", {
 
 
 
-let container = document.querySelector(".products-results");
-let maskContainer = document.querySelector(".mask-container");
-let maskImageContainer = document.querySelector(".mask-image-container");
+// let container = document.querySelector(".products-results");
+// let maskContainer = document.querySelector(".mask-container");
+// let maskImageContainer = document.querySelector(".mask-image-container");
 
-let border = document.querySelector(".border");
-let circle = document.querySelector(".circle");
+// let border = document.querySelector(".border");
+// let circle = document.querySelector(".circle");
 
-circle.style.draggable = true;
+// circle.style.draggable = true;
 
-circle.ondrag = function(event){
-    maskContainer.style.width = event.pageX + "px";
-    border.style.left = event.pageX + "px";
-    circle.style.left = event.pageX + "px";
-}
+// circle.ondrag = function(event){
+//     maskContainer.style.width = event.pageX + "px";
+//     border.style.left = event.pageX + "px";
+//     circle.style.left = event.pageX + "px";
+// }
 
-circle.ondragend = function(event){
-    maskContainer.style.width = event.pageX + "px";
-    border.style.left = event.pageX + "px";
-    circle.style.left = event.pageX + "px";
-}
+// circle.ondragend = function(event){
+//     maskContainer.style.width = event.pageX + "px";
+//     border.style.left = event.pageX + "px";
+//     circle.style.left = event.pageX + "px";
+// }
 
 
 
